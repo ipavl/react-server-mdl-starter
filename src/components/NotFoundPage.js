7d8b88f0e78d7aceeeff2b51d315b3ad3577f189
ipavl <ipavl@users.noreply.github.com>
2016-10-07 22:12:57 -0400

    Create basic client-side React site

---
+++ src/components/NotFoundPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class NotFoundPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Page not found</h1>
+
+        <p>The requested page could not be found.</p>
+      </div>
+    );
+  }
+}
---
+++ src/data/projects.js
@@ -0,0 +1,12 @@
+const projects = [
+  {
+    name: 'reactsite',
+    description: 'A site built with React.',
+    tags: [
+      'React',
+      'JavaScript',
+    ],
+  },
+];
+
+export default projects;
