4a6e665845d4691ff1a4920439cefd59cfb3726e
ipavl <ipavl@users.noreply.github.com>
2017-01-08 00:33:45 -0500

    Add page to view individual project details

---
+++ src/components/ProjectDetailsPage.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { List, ListItem, ListItemContent } from 'react-mdl';
+import projects from '../data/projects';
+import NotFoundPage from './NotFoundPage';
+
+export default class ProjectDetailsPage extends React.Component {
+  getProjectDetails() {
+    for (let i = 0; i < projects.length; i++) {
+      if (projects[i].slug === this.props.params.slug) {
+        return projects[i];
+      }
+    }
+
+    return null;
+  }
+
+  render() {
+    let project = this.getProjectDetails();
+
+    if (project === null) {
+      // TODO: Send back a 404 status code
+      return <NotFoundPage />;
+    }
+
+    return (
+      <div>
+        <h1>{project.name}</h1>
+
+        <div>{project.description}</div>
+
+        <h2>Tags</h2>
+
+        <List>
+          {project.tags.map(tagData =>
+            <ListItem key={tagData}>
+              <ListItemContent>
+                {tagData}
+              </ListItemContent>
+            </ListItem>
+          )}
+        </List>
+      </div>
+    );
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import { Route, IndexRoute } from 'react-router';
 import PageLayout from './components/PageLayout';
 import IndexPage from './components/IndexPage';
 import ProjectsPage from './components/ProjectsPage';
+import ProjectDetailsPage from './components/ProjectDetailsPage';
 import NotFoundPage from './components/NotFoundPage';
 
 const routes = (
   <Route path="/" component={PageLayout}>
     <IndexRoute component={IndexPage} />
     <Route path="/projects" component={ProjectsPage} />
+    <Route path="/projects/:slug" component={ProjectDetailsPage} />
     <Route path="*" component={NotFoundPage} />
   </Route>
 );
