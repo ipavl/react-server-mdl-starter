b94bb095e887deb3af414a27af8e8ff04bb66564
ipavl <ipavl@users.noreply.github.com>
2017-01-08 09:54:52 -0500

    Fix indentation

--- src/components/Projects/ProjectListItem.js
+++ src/components/Projects/ProjectListItem.js
@@ -5,11 +5,11 @@ import { ListItem, ListItemContent } from 'react-mdl';
 export default class ProjectListItem extends React.Component {
   render() {
     return (
-        <ListItem threeLine>
-          <ListItemContent subtitle={this.props.description}>
-            <Link to={`/projects/${this.props.slug}`}>{this.props.name}</Link>
-          </ListItemContent>
-        </ListItem>
+      <ListItem threeLine>
+        <ListItemContent subtitle={this.props.description}>
+          <Link to={`/projects/${this.props.slug}`}>{this.props.name}</Link>
+        </ListItemContent>
+      </ListItem>
     );
   }
 }
